109eef6d2306c43257a332db430101a2d62ea922
Devin Polanco Romero <72811716+Devin001338@users.noreply.github.com>
2023-10-02 23:07:10 +0000

    compilador de archivos automatico

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+ 
+javac *.java 
 
-# Compilar todos los archivos Java en la carpeta
-javac *.java
-
-# Mover los archivos .class a una carpeta separada (opcional)
 mkdir -p java-class
 mv *.class java-class/
